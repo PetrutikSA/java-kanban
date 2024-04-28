077a63cc0e1698375fde7e7bb718e5e0b3e859e7
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-04-28 16:28:59 +0300

    feat: создать обработчики и привязать к соответствующим путям

--- src/HttpTaskServer.java
+++ src/HttpTaskServer.java
@@ -1,4 +1,5 @@
 import com.sun.net.httpserver.HttpServer;
+import handlers.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -7,6 +8,11 @@ public class HttpTaskServer {
     public static void main(String[] args) {
         try {
             HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080),0);
+            httpServer.createContext("/tasks", new TaskHandler());
+            httpServer.createContext("/epics", new EpicHandler());
+            httpServer.createContext("/subtasks", new SubtaskHandler());
+            httpServer.createContext("/history", new HistoryHandler());
+            httpServer.createContext("/prioritized", new PrioritizedHandler());
             httpServer.start();
         } catch (IOException e) {
             throw new RuntimeException(e);
