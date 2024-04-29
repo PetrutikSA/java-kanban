6182d37b762a2b260b558eab1361d3faa04bb3db
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-04-29 13:47:23 +0300

    feat: реализовать HttpTaskServer конструктор, методы start/stop

--- src/HttpTaskServer.java
+++ src/HttpTaskServer.java
@@ -1,5 +1,9 @@
 import com.sun.net.httpserver.HttpServer;
-import handlers.*;
+import handlers.EpicHandler;
+import handlers.HistoryHandler;
+import handlers.PrioritizedHandler;
+import handlers.SubtaskHandler;
+import handlers.TaskHandler;
 import managers.Managers;
 import managers.tasks.TaskManager;
 
@@ -7,14 +11,31 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class HttpTaskServer {
+    private final TaskManager taskManager;
+    private HttpServer httpServer;
+
+    public HttpTaskServer(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
+
     public static void main(String[] args) throws IOException {
-        TaskManager taskManager = Managers.getDefault();
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080),0);
+        HttpTaskServer httpTaskServer = new HttpTaskServer(Managers.getDefault());
+        httpTaskServer.start(8080);
+        System.out.println("Сервер запущен");
+    }
+
+    public void start(int port) throws IOException {
+        httpServer = HttpServer.create(new InetSocketAddress(port),0);
         httpServer.createContext("/tasks", new TaskHandler(taskManager));
         httpServer.createContext("/epics", new EpicHandler(taskManager));
         httpServer.createContext("/subtasks", new SubtaskHandler(taskManager));
         httpServer.createContext("/history", new HistoryHandler(taskManager));
         httpServer.createContext("/prioritized", new PrioritizedHandler(taskManager));
         httpServer.start();
+    }
+
+    public void stop() {
+        httpServer.stop(0);
+        System.out.println("Сервер остановлен");
     }
 }
